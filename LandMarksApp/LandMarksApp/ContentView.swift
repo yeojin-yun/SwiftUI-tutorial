ee1fe3cc6e5e7a03a03116056c55c2530b326b82
yeojinyun <sail041@naver.com>
2023-10-30 22:00:34 +0900

    LandMark App Complete

--- LandMarksApp/LandMarksApp/ContentView.swift
+++ LandMarksApp/LandMarksApp/ContentView.swift
@@ -9,17 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Turtle Rock")
                 .font(.title)
             HStack {
                 Text("Joshua Tree National Park")
                     .font(.subheadline)
+                Spacer()
                 Text("California")
-                    .font(.subheadline)
-            }
-        }
             
+            }
+            .padding(20)
+        }
+        .padding()
     }
 }
 
