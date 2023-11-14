76b51641ec9a912a7b049bcc305c7fb7ae128ca4
yeojinyun <sail041@naver.com>
2023-11-14 21:43:28 +0900

    complete proj

--- LandMarksApp/LandMarksApp/ContentView.swift
+++ LandMarksApp/LandMarksApp/ContentView.swift
@@ -13,21 +13,28 @@ struct ContentView: View {
             MapView()
                 .frame(height: 300)
             CircleImage()
-//                .offset(y: -130)
-//                .padding(.bottom, -130)
+                .offset(y: -130)
+                .padding(.bottom, -130)
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
                 HStack {
                     Text("Joshua Tree National Park")
-                        .font(.subheadline)
+                        
                     Spacer()
                     Text("California")
-                
                 }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
             .padding()
+            Spacer()
         }
+        
     }
 }
 
