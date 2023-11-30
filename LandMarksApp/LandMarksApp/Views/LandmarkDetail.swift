6082fa79f0f818daacc12a037706061bbf373dec
yeojinyun <sail041@naver.com>
2023-11-30 23:13:05 +0900

    add Landmark Detail

---
+++ LandMarksApp/LandMarksApp/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  LandMarksApp
+//
+//  Created by 순진이 on 11/30/23.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
